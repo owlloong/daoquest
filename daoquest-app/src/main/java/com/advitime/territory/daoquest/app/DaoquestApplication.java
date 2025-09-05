38c56b35da6a5456d839a2bfd98758402f1bec99
owlloongyuan <owlloong@foxmail.com>
2025-09-05 13:20:04 +0800

    feat: 初始化项目，清除旧历史

---
+++ daoquest-app/src/main/java/com/advitime/territory/daoquest/app/DaoquestApplication.java
@@ -0,0 +1,20 @@
+package com.advitime.territory.daoquest.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName DaoquestApplication
+ * @Description TODO
+ * @Author owlloongyuan
+ * @Date 2025/9/5 10:28
+ * @Version 0.1
+ */
+@SpringBootApplication
+public class DaoquestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaoquestApplication.class, args);
+    }
+
+}
